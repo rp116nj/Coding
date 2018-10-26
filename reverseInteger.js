da7ada38e3236949520a417d21dfa560680ca9a0
rp116nj <37672253+rp116nj@users.noreply.github.com>
2018-10-26 12:02:34 -0400

    Add files via upload

---
+++ reverseInteger.js
@@ -0,0 +1,18 @@
+// --- Directions
+// Given an integer, return an integer that is the reverse
+// ordering of numbers.
+// --- Examples
+//   reverseInt(15) === 51
+//   reverseInt(981) === 189
+//   reverseInt(500) === 5
+//   reverseInt(-15) === -51
+//   reverseInt(-90) === -9
+
+
+function reverseInt(num){
+//all these methods work only on strings. hence converting to string
+  revNum = String(num).split('').reverse().join('');//converting the number to string, splitting ,reversing and joining 
+  console.log(revNum);
+}
+
+reverseInt(15);
